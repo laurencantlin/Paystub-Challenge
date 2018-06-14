16967e581abde0be863728edf039f22d96452a02
laurencantlin <laurenrose5321@gmail.com>
2018-06-14 16:41:25 -0400

    adds next js basic code

---
+++ client/src/pages/Paystub/Paystub.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+
+class Paystub extends Component {
+    render() {
+        return ( <div> paystub </div>
+        )
+    }
+}
+export default Paystub;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// This is the Link API
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/Paystub">
+      <a>Paystub Page</a>
+    </Link>
+    <p>Paystub.js</p>
+  </div>
+)
+
+export default Index
